0336f77502e6630134c7427d63667060a4928c39
Craig Dolan <craig@retryabortfail.com>
2016-07-05 14:49:39 +0100

    Number practice

--- chapter2/birthday.py
+++ chapter2/birthday.py
@@ -1,3 +1,12 @@
 age = 23
 message = "Happy " + str(age) + "rd Birthday!"
-print (message)
+print(message)
+
+print(2+4)
+print(10-2)
+print(4*2)
+print(16/2)
+
+best_number = 13
+number_message = "The best number is " + str(best_number)
+print(number_message)
