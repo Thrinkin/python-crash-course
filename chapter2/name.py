c8c97a1cd423c83ab8c2d48e593a55d8ee18f4ec
Craig Dolan <craig@retryabortfail.com>
2016-07-05 14:25:36 +0100

    Variable rather than directly create  it

--- chapter2/name.py
+++ chapter2/name.py
@@ -9,4 +9,5 @@ first_name = "ada"
 last_name = "lovelace"
 full_name = first_name + " " + last_name
 print(full_name)
-print("Hello, " + full_name.title() + "!")
+message = "Hello, " + full_name.title() + "!"
+print(message)
